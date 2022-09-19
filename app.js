6a555f0c43af091870d8be4dcb355aa350fef67e
carlosesconav <carlosesconav@gmail.com>
2022-09-19 16:01:50 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const { crearArchivo } = require('./helpers/multiplicar');
+
+
+console.log(process.argv);
+const [, , arg3 = 'base=5'] = process.argv;
+const [, base = 5 ] = arg3.split('=')
+console.log(base)
+
+// const base = 7;
+
+
+
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo, 'ha sido creado'))
+    .catch(err => console.log(err));
+
